f2d9e58340d1cd8149ee0eb904a8f53bda003bb9
RYM1999 <rohitmungase1@gmail.com>
2021-04-07 13:51:09 +0530

    first commit

---
+++ php/navbar.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <style>
+    a:link {
+      color: white;
+      text-decoration: none;
+    }
+
+    a:hover {
+      color: white;
+      text-decoration: none;
+    }
+
+    .home{
+  background-color:blue;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+  opacity: 0.9;
+  
+}
+
+.view{
+  background-color:#FF4500;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+  opacity: 0.9;
+}
+
+
+
+.transactions{
+  background-color:blue;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+  opacity: 0.9;
+}
+
+  </style>
+</head>
+
+<body>
+  
+  
+    
+<p align="center"> 
+      </br></br>
+        <a href="../index.php" style="text-decoration:none; background:#254AA5" class="home">Home</a>
+        <a href="transfermoney.php" style="text-decoration:none; background:#00A650" class="view">View Customers</a>
+        <a href="transactionhistory.php" style="text-decoration:none; background:#a60085" class="transactions">Transactions</a>
+      
+    
+
+  
+</body>
+
+</html>
